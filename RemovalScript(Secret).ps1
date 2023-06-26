327a8822abdac611c0a1a1b71042da70ca376071
TheAmazingAve <89434577+AveryJHQ@users.noreply.github.com>
2023-06-26 11:52:17 -0400

    Create RemovalScript(Secret).ps1
    Removal Script

---
+++ RemovalScript(Secret).ps1
@@ -0,0 +1,17 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+$App = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Secret Server Protocol Handler"}
+$App.Uninstall()
